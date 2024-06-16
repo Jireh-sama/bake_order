a03e8db5ee0998d865606dbc15473f8d07144c03
ecchiko <jirehtumbagahan@gmail.com>
2024-06-16 17:33:30 +0800

    refactor: update variable name and minor ui changes

--- frames/landing_frame.py
+++ frames/landing_frame.py
@@ -2,11 +2,11 @@ import customtkinter as ctk
 from utils.ctk_utils import create_frame, create_label, create_image
 
 class LandingFrame(ctk.CTkFrame):
-  def __init__(self, parent, open_customer, open_owner):
+  def __init__(self, parent, open_buy, open_view_receipt):
     super().__init__(parent)
 
-    self.open_customer = open_customer
-    self.open_owner = open_owner
+    self.open_buy = open_buy
+    self.open_view_receipt = open_view_receipt
 
     self.create_widgets()
 
@@ -17,17 +17,19 @@ class LandingFrame(ctk.CTkFrame):
     self.login_frame = create_frame(parent=self, width=400, height=650, bg_color="#E49748")
     self.login_frame.pack(side="left", fill="both", expand=True)
 
-    self.header_label = create_label(self.login_frame, "Get Started by Choosing your Role");
-    self.header_label.pack()
+    self.header_label = create_label(self.login_frame, "Shop Now")
+    self.header_label.configure(font=("Arial", 34))
+    self.header_label.pack(pady=(25, 0))
 
-    self.owner_btn = ctk.CTkButton(self.login_frame, text="Owner", command=self.open_owner, width=241, height=50, fg_color="#6C3A30", corner_radius=20)
-    self.owner_btn.pack()
+    self.customer_btn = ctk.CTkButton(self.login_frame, text="Buy", command=self.open_buy, width=241, height=50, fg_color="#6C3A30", corner_radius=20)
+    self.customer_btn.pack(pady=(195, 0))
 
-    self.text_label = create_label(self.login_frame, "or")
-    self.text_label.pack()
+    self.owner_btn = ctk.CTkButton(self.login_frame, text="View Receipt", command=self.open_view_receipt, width=241, height=50, fg_color="#6C3A30", corner_radius=20)
+    self.owner_btn.pack(pady=(25, 0))
 
-    self.customer_btn = ctk.CTkButton(self.login_frame, text="Customer", command=self.open_customer, width=241, height=50, fg_color="#6C3A30", corner_radius=20)
-    self.customer_btn.pack()
+    
+
+    
 
     self.image_label = create_image(self.cover_frame, path="./assets//media//bake-order-logo.png", width=280, height=200)
     self.image_label.pack(expand=True)
